21fc4e321d58e740a7d95d26e88bc0f67d4953dc
Richard Joubert <richard@nsd.academy>
2022-07-18 17:14:15 -0400

    update Declarations.cs

--- Testing/Variables/Basics/Declarations.cs
+++ Testing/Variables/Basics/Declarations.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using System;
 
 namespace DataTypes
@@ -7,45 +7,53 @@ namespace DataTypes
 	{
 		static void Main(string[] args)
 		{
-	/*===|  Declaration Examples  |===*/
-		// Numeric Data Types
-		int a = 75;
-		double b = 7.258;
-		long c = 67893342128382;
+/*		Declaration Examples
+Numeric Data Types*/
+		/// Integers: Whole numbers
+			int a = 75;
+		
+		
+			double b = 7.258;
+		
+		
+			long c = 67893342128382;
 		
 		// Text-based Data Types
-		string ChaCha = "I do the Cha-Cha like a Sissy Girl.";
-		char Nuts = 'n';
+			string ChaCha = "I do the Cha-Cha like a Sissy Girl.";
+		
+		
+			char Nuts = 'n';
 		
 		// Boolean
 		bool Result = true;
 		
 	/*===|  Output Examples  |===*/
 		// Numeric Outputs
-		Console.WriteLine("\n Text/String Output" + "\n");
 		Console.WriteLine("\n'a' = " + a + "\n");
 		Console.WriteLine("\n'b' = " + b + "\n");
 		Console.WriteLine("\n'c' = " + c + "\n");
 		
-		// Simple Arithmetic
-		Console.WriteLine("\na ⨯ b = ", a * b, "\n");
-		Console.WriteLine("\nc ÷ b = ", c / b, "\n");
-		Console.WriteLine("\nb + b = ", c + b, "\n");
-		Console.WriteLine("\nb - a + b = ", c - a + b, "\n");
+		// Numeric Outputs v2
+		Console.WriteLine("\nNumeric/Integer Data Types:\n");
+		Console.WriteLine("\tInteger\tType\tValue\n");
+		Console.WriteLine("'a'\t\t");
+			Console.Write(c);
+			Console.Write("\t");
+			
 		
 		// Simple Arithmetic v2
 		Console.Write("\na ⨯ b = ");
-			Console.WriteLine(a * <b>, "\n");
-		Console.Write("\nc ÷ b = ");
-			Console.WriteLine(c / b, "\n");
-		Console.Write("\nb + b = ");
-			Console.WriteLine(c + b, "\n");
-		Console.Write("\nb - a = ");
-			Console.WriteLine(c + b, "\n");
+			Console.Write(a * b);
+		Console.Write("\n\nc ÷ b = ");
+			Console.Write(c / b);
+		Console.Write("\n\nb + b = ");
+			Console.WriteLine(c + b);
+		Console.Write("\n\nb - a = ");
+			Console.WriteLine(c + b);
 			
-		
-		Console.Write("\nb - a + b = ");
-			Console.WriteLine("\nb - a + b = ", c - a + b, "\n");
+		// Multi-Step Equations
+		Console.Write("\n\nb - a + b = ");
+			Console.WriteLine(c - a + b);
 		
 		
 		// Text Output
