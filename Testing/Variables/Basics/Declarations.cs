8fd37aad5658d524c1a262a5b312c6527bae80c5
Richard Joubert <richard@nsd.academy>
2022-07-18 13:55:48 -0400

    update Declarations.cs

--- Testing/Variables/Basics/Declarations.cs
+++ Testing/Variables/Basics/Declarations.cs
@@ -1,11 +1,36 @@
 using System;
 
-namespace VarTypes
+namespace DataTypes
 {
-	class Declarationa
+	class Declarations
 	{
-		string = "I do the Cha-Cha like a Sissy Girl."
-		int = 26
-		double = 7.25
+		static void Main(string[] args);
+		{
+	/*===|  Declaration Examples  |===*/
+		// Numeric Data Types
+		int x = 75
+		double y = 7.258
+		long z = 67,893,342,128,382
+		
+		// Text-based Data Types
+		string ChaCha = "I do the Cha-Cha like a Sissy Girl."
+		char Nuts = 'n'
+		
+		// Boolean
+		bool Result = true
+		
+	/*===|  Output Examples  |===*/
+		// Numeric Outputs
+		Console.WriteLine(x)
+		Console.WriteLine(y)
+		Console.WriteLine(z)
+		
+		// Text Outputs
+		Console.WriteLine(ChaCha)
+		Console.WriteLine(Nuts)
+		
+		// Text Outputs
+		Console.WriteLine("Result: " + Result)
+	}
 	}
 }
