98d412fe9487ddea50433bb3c63794bcff062d98
Richard Joubert <richard@nsd.academy>
2022-07-18 14:27:03 -0400

    update Declarations.cs

--- Testing/Variables/Basics/Declarations.cs
+++ Testing/Variables/Basics/Declarations.cs
@@ -1,39 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
 using System;
 
 namespace DataTypes
 {
-	class Declarations
+	class Program
 	{
-		static void Main(string[] args);
+		static void Main(string[] args)
 		{
 	/*===|  Declaration Examples  |===*/
 		// Numeric Data Types
-		int x = 75
-		double y = 7.258
-		long z = 67,893,342,128,382
+		int a = 75;
+		double b = 7.258;
+		long c = 67,893,342,128,382;
 		
 		// Text-based Data Types
-		string ChaCha = "I do the Cha-Cha like a Sissy Girl."
-		char Nuts = 'n'
+		string ChaCha = "I do the Cha-Cha like a Sissy Girl.";
+		char Nuts = 'n';
 		
 		// Boolean
-		bool Result = true
+		bool Result = true;
 		
 	/*===|  Output Examples  |===*/
 		// Numeric Outputs
-		Console.WriteLine(x)
-		Console.WriteLine(y)
-		Console.WriteLine(z)
+		Console.WriteLine(a);
+		Console.WriteLine(b);
+		Console.WriteLine(c);
 		
-		Console.WriteLine(x * y)
-		Console.WriteLine(x / y)
-		
-		// Text Outputs
-		Console.WriteLine(ChaCha)
-		Console.WriteLine(Nuts)
+		Console.WriteLine(a * b);
+		Console.WriteLine(a / b);
 		
 		// Text Outputs
-		Console.WriteLine("Result: " + Result)
+		Console.WriteLine(ChaCha);
+		Console.WriteLine(Nuts);
+		
+		// Text Outputs
+		Console.WriteLine("Result: " + Result);
 		}
 	}
 }
